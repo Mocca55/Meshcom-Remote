9dbe18d384b6a8ae4cc931ed888017488dacbd22
Mocca55 <65739996+Mocca55@users.noreply.github.com>
2026-04-04 16:37:06 +0200

    Readme aktualisiert

--- Meshcom-Remote-Admin.ino
+++ Meshcom-Remote-Admin.ino
@@ -1,7 +1,37 @@
+// RX TX LEDs schaltbar machen
+
+void ledTx( boolean on)
+{
+  if( on)
+  {
+    // LED an
+    pinMode( LED_BUILTIN_TX, OUTPUT);    // TX LED an
+    digitalWrite( LED_BUILTIN_TX, LOW);  // TX LED aus    
+  }
+  else
+  {
+    pinMode( LED_BUILTIN_TX, INPUT);
+  }
+}
+
+void ledRx( boolean on)
+{
+  if( on)
+  {
+    pinMode( LED_BUILTIN_RX, OUTPUT);  // RX LED an
+    digitalWrite( LED_BUILTIN_RX, LOW); // RX LED aus
+  }
+  else
+  {
+    pinMode( LED_BUILTIN_RX, INPUT);    
+  }
+}
+
+//Reset Pin am Arduino Micro Pro zuweisen Pin7
 const int resetPin = 7;
 
 // RX Befehle und Status Infos
-const char* cmdReboot = "CALL-1>CALL-2:remotereboot"; //Amateurfunk CALL Sender und Empfänger einsetzen
+const char* cmdReboot = "CALL-1>CALL-2:remotereboot";  //hier müssen die Rufzeichen der Meshcom Nodes eingetragen werden Rufzeichen 1 Sender Rufzeichen 2 Empfänger (zu Administrierender Node)
 const char* cmdStarted = "CLIENT STARTED";
 
 // Befehle definieren für Prgramm
@@ -9,14 +39,23 @@ int posReboot = 0;
 int posStarted = 0;
 
 void setup() {
-  // Baudrate muss gleich sein bei Serial und Serial1 (ESP32 115200)
+
+
+// LEDs abschalten
+  delay(500);
+  ledTx( false);
+  delay(500);
+  ledRx( false);
+
+
+  // Baudrate muss ggleich sein bei Serial und Serial1 (ESP32 115200)
   Serial.begin(115200);
   Serial1.begin(115200);
 
   pinMode(resetPin, OUTPUT);
   digitalWrite(resetPin, HIGH);
 
-  while (!Serial);
+  //while (!Serial); //abschalten bei Nutzung ohne PC
   Serial.println("[Info Arduino] Überwachung aktiv (RX Pin vom Lilygo Lora32 ESP wird ausgelesen)...");
 }
 
@@ -46,6 +85,17 @@ void loop() {
       if (cmdStarted[posStarted] == '\0') {
         Serial.println("\n----------\n[Info Arduino] Lora ESP32 Node gestartet und bereit.\n----------");
         posStarted = 0;
+          ledRx( true);
+          delay(200);
+          ledRx( false);
+          delay(200);
+          ledRx( true);
+          delay(200);
+          ledRx( false);
+          delay(200);
+          ledRx( true);
+          delay(200);
+          ledRx( false);
       }
     } else {
       posStarted = (c == cmdStarted[0]) ? 1 : 0;
@@ -58,6 +108,13 @@ void executeReset() {
   digitalWrite(resetPin, LOW);
   delay(100);
   digitalWrite(resetPin, HIGH);
+          ledRx( true);
+          delay(200);
+          ledRx( false);
+          delay(200);
+          ledRx( true);
+          delay(200);
+          ledRx( false);
   // Nach Reset kurz warten, um Datenmüll während des Neustarts zu ignorieren
   posReboot = 0;
   posStarted = 0;
