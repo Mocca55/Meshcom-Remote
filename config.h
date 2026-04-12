ff7943183cf9d912d6705fca139d112d3ab150fd
Michael <65739996+Mocca55@users.noreply.github.com>
2026-04-12 21:08:45 +0200

    Add files via upload

---
+++ config.h
@@ -0,0 +1,5 @@
+//---------- Setup Main und Remote Node --------- Hier müssen die Rufzeichen der Nodes Call1 Main Node und Call2 RemoteNode zu Administrierender Node eingetragen werden.
+
+String MainNode = ("CALL-1");
+
+String RemoteNode = ("CALL-2");
