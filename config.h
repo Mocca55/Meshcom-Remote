5da5f8db495d298f2d084b7e1b1ce9e65b05ee64
Michael <65739996+Mocca55@users.noreply.github.com>
2026-04-12 21:10:00 +0200

    Update config.h
    Added comments to clarify MainNode and RemoteNode.

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 //---------- Setup Main und Remote Node --------- Hier müssen die Rufzeichen der Nodes Call1 Main Node und Call2 RemoteNode zu Administrierender Node eingetragen werden.
 
-String MainNode = ("CALL-1");
+String MainNode = ("CALL-1"); //Main Node
 
-String RemoteNode = ("CALL-2");
+String RemoteNode = ("CALL-2"); //Remote Node
