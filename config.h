ac8032caecc66b4f2130be984516617a3fa6e0c6
Michael <65739996+Mocca55@users.noreply.github.com>
2026-04-12 21:22:54 +0200

    Update config.h
    Updated MainNode and RemoteNode identifiers for configuration.

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 //---------- Setup Main und Remote Node --------- Hier müssen die Rufzeichen der Nodes Call1 Main Node und Call2 RemoteNode zu Administrierender Node eingetragen werden.
 
-String MainNode = ("DK6MM-1"); //Main Node
+String MainNode = ("CALL-1"); //Main Node
 
-String RemoteNode = ("DK6MM-2"); //Remote Node
+String RemoteNode = ("CALL-2"); //Remote Node
 
 String RemoteVersion = ("Meshcom Remote V1.0.2"); //Versionsinfo
